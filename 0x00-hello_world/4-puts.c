9f17b3de3dcdba9b7c8c2d6f2ef3c85369288197
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-08 09:50:33 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc $CFILE -o hello_put
+#include <stdio.h>
+
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzel, followed by a new line
+ *
+ * Return: ALways 0
+ */
+in main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
