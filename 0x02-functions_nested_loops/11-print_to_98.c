31f41d3af332f085e17db5d7e58f34a76f3b4806
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-14 11:49:29 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,20 +2,21 @@
 
 /**
  * print_to_98 - print all natural numbers from input to 98,
- * 		in order separated by a comma followed by a space.
+ *		in order separated by a comma followed by a space.
+ * @n: The number to begin at
  */
 void print_to_98(int n)
 {
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d ,", n--);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-			printf("%d ,", n++);
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 }
