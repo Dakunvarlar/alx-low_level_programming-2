50a4a435bcf1c5c77948494c2830234757d72f83
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-15 02:28:26 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ int main(void)
 
 	i = o;
 
-	/*while (i < 10)
-	{
-		putchar(i);
-	}*/
-
+/*	while (i < 10)
+*	{
+*		putchar(i);
+*	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
