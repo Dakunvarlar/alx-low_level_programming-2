5ee48aaf1cc34104dfe2bea0f11c477f98a09989
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-16 07:33:09 -0700

    isadigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * isadigit - checks whether character is a digit
+ * @c: is to be used as argument
+ * Return: 1 on success
+ * Return: 0 on error
+ */
+int _isdigit(int c)
+{
+	int c;
+
+	if (int c == (int))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
