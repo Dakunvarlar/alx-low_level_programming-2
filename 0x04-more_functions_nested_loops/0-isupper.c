f8e8db2cd25799e04d9bdd3515e8b50315239bbb
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-16 04:05:28 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _is alpha - function checks if c iuppercase
+ * Return: 1 if c is uppercase
+ * 	otherwise return 0
+ */
+int _isupper(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
