8fd96b2c34896bfdc6da790fcd4781b49cbdec18
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-19 03:24:16 -0700

    Do not swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*this is a putchar function*/
 int _putchar(char c);
+/*this function change the value it points to 98*/
 void reset_to_98(int *n);
+/*this function swaps the value of two integers*/
 void swap_int(int *a, int *b);
+/*this function returns the length of a string*/
 int _strlen(char *s);
+/*this function prints the length of a string*/
 void _puts(char *str);
+/*this function prints a string*/
 void print_rev(char *s);
+/*this function prints a string in reverse*/
 void rev_string(char *s);
+/*this function prints every other character*/
 void puts2(char *str);
+/*this function prints half of a string*/
 void puts_half(char *str);
+/*this function prints n elements of an array*/
 void print_array(int *a, int n);
+/*this functioncpies from a pointer to another*/
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H*/
