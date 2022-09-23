5d673033547d00a11570cf2a029c5d550d433de8
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-23 10:26:44 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,12 +16,12 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (ii = 0; ii < 52; ii++)
+		for (ii = 0; ii < 54; ii++)
 		{
 			if if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
 					&& s[i] == input[ii])
 			{
-				s[i] = datarot[ii];
+				s[i] = output[ii];
 				break;
 			}
 		}
