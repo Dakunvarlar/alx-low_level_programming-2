f0040f29ff78027ce6fda4eecb6007487897a596
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-25 04:00:31 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,27 +1,31 @@
-#include "main'h"
 #include <stdio.h>
+
 /**
- * main - this program prints every minute
- *
+ * main - computes and prints the sum of all the multiples
+ * of 3 and 5 below 1024
  * Return: sum
  */
 int main(void)
 {
-	int n = 0;
-	int sum = 0;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	while (n < 1024)
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for ( i = 0; i < 1024; ++i)
 	{
-		if (n % 3 == 0)
+		if ((i % 3) == 0)
 		{
-			sum += n;
+			sum3 = sum3 + 1;
 		}
-		else if (n % 5 == 0)
+		else if ((i % 5) == 0)
 		{
-			sum += n;
+			sum5 = sum5 + i;
 		}
-		n++;
 	}
-	printf("%d\n", sum);
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
 	return (0);
 }
