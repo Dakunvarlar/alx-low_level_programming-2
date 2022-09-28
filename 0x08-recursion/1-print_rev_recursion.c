1777c4f4528f7251b6b5010edfc958737b2b990b
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-28 04:03:00 -0700

    Why is it so important to dream

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a string
+ * @s: source string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (!s || !*s)
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
