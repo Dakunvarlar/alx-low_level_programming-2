8a93a2a924dd00ab9ae8ab828c2ace7fc7dc5142
Dakunvarlar <dakunvarlar@gmail.com>
2022-09-30 06:52:06 -0700

    it is what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - this program prints its name
+ *
+ * @argc: number of arguments
+ * @argv: string of arguments
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[o]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the task on the argc (argument count) and argv (argument vector)
