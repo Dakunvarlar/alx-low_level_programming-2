9bdef014bc19fbb69fa202f78fe1936c16f6ac94
Dakunvarlar <dakunvarlar@gmail.com>
2022-10-07 04:39:10 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
@@ -18,18 +19,21 @@ void _is_zero(char *argv[])
 			isn1 = 0;
 			break;
 		}
+
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
 			isn2 = 0;
 			break;
 		}
+
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
 	}
 }
+
 /**
  * _initialize_array - set memory to zero in a new array
  * @ar: char array.
@@ -65,6 +69,8 @@ int _checknum(char *argv[], int n)
 			printf("Error\n");
 			exit(98);
 		}
+
+
 	return (ln);
 }
 
