52bb8ee2b37d7571f781a589f597b6a64fed2405
Dakunvarlar <dakunvarlar@gmail.com>
2022-10-07 02:44:12 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		complete = malloc(len1 + 1 + (sizeof(char) * n));
-		if (complete == NUUL)
+		if (complete == NULL)
 			return (NULL);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
