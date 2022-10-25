922810535e86303a9c00c1a4017c605a6079f66f
Dakunvarlar <dakunvarlar@gmail.com>
2022-10-24 18:02:43 -0700

    Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - this function adds every n data of every node
+ * @head: pointer to list
+ * Return: sum of all n data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
